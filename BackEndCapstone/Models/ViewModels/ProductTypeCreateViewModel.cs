1f23cb4785d44dad87c40c46ccd63945232610e8
aryn weatherly <arynweatherly93@gmail.com>
2020-02-08 19:43:47 -0600

    still trying to get products comments

---
+++ BackEndCapstone/Models/ViewModels/ProductTypeCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndCapstone.Models.ViewModels
+{
+    public class ProductTypeCreateViewModel
+    {
+    }
+}
