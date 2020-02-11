0342c6be273970e4766312ad366df2e1a1bd228a
aryn weatherly <arynweatherly93@gmail.com>
2020-02-11 11:04:42 -0600

    name shows up on product review

---
+++ BackEndCapstone/Models/ViewModels/ProductReviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndCapstone.Models.ViewModels
+{
+    public class ProductReviewViewModel
+    {
+        public Tutorial Tutorial { get; set; }
+        public List<ProductReview> ProductReviews { get; set; }
+        public string Comment { get; set; }
+        public ApplicationUser User { get; set; }
+        public string UserId { get; set; }
+        public string Email { get; set; }
+    }
+}
